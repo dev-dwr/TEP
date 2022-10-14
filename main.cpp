83c05aa7240fd3c06d362054895037d6833fa7ff
dev-dwr <dawid.jscript@gmail.com>
2022-10-14 15:25:23 +0200

    fixed issues related to 3 star pointer.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include "main.h"
-//alokacja statyczna ma miejsce kiedy deklarujemy zmienna
-//Znak & oznacza referencję do obiektu/zmiennej. Tak więc zmienna &pcOther jest referencją
-//do typu CAllocTest. Referencja to wskaźnik
 
+//zadanie 1
 void v_alloc_table_fill_34(int iSize) {
     if (iSize < 0) {
         std::cout << "Size needs to be a positive number" << std::endl;
@@ -23,7 +21,8 @@ void v_alloc_table_fill_34(int iSize) {
     delete[] array;
 }
 
-bool b_alloc_table_2dim(int **&piTable, int iSizeX, int iSizeY) {
+//zadanie 2
+bool b_alloc_table_2dim(int ***piTable, int iSizeX, int iSizeY) {
     if (iSizeX < 0) {
         std::cout << "row number must be positive" << std::endl;
         return false;
@@ -32,20 +31,16 @@ bool b_alloc_table_2dim(int **&piTable, int iSizeX, int iSizeY) {
         std::cout << "column number must be positive" << std::endl;
         return false;
     }
-    if (piTable == NULL) {
-        std::cout << " pi_table null value passed" << std::endl;
-        return false;
-    }
 
-    piTable = new int *[iSizeX]; //allocate row
+    *piTable = new int *[iSizeX]; //allocate row
     for (int i = 0; i < iSizeX; i++) {
-        piTable[i] = new int[iSizeY]; //allocate column
+        (*piTable)[i] = new int[iSizeY]; //allocate column
     }
 
     return true;
 }
 
-void allocate_array_data(int **&piTable, int iSizeX, int iSizeY) {
+void allocate_array_data(int **piTable, int iSizeX, int iSizeY) {
     for (int i = 0; i < iSizeX; i++) {
         for (int j = 0; j < iSizeY; j++) {
             piTable[i][j] = 5;
@@ -53,33 +48,27 @@ void allocate_array_data(int **&piTable, int iSizeX, int iSizeY) {
     }
 }
 
-bool b_dealloc_table_2_dim(int **piTable, int iSizeX) {
-    if (iSizeX != I_SIZE_X) {
+//zadnie 3
+bool b_dealloc_table_2_dim(int ***piTable, int iSizeX, int iSizeY) {
+    if (iSizeY < 0){
+        std::cout << "cannot deallocate array wrong number column passed" << std::endl;
+        return false;
+    }
+    if (iSizeX < 0 ) {
         std::cout << "cannot deallocate array wrong number row passed" << std::endl;
         return false;
     }
-    if (piTable == NULL) {
-        std::cout << "null value passed" << std::endl;
-        return false;
-    }
-    //delete piTable;. Skasuje ono wyłącznie tablicę wskaźników
+
+    //delete piTable; Skasuje ono wyłącznie tablicę wskaźników
     for (int i = 0; i < iSizeX; i++) {
-        delete[] piTable[i]; //delete all rows in every column
+        delete[] (*piTable)[i]; //delete all rows in every column
     }
 
-    delete[] piTable; // delete columns
+    delete[] *piTable; // delete columns
     return true;
 }
 
-void display_array(int **&piTable, int iSizeX, int iSizeY) {
-    for (int i = 0; i < iSizeX; i++) {
-        for (int j = 0; j < iSizeY; j++) {
-            std::cout << piTable[i][j] << "\t";
-        }
-        std::cout << std::endl;
-    }
-}
-
+//zadanie 4
 class CTable {
 private:
     std::string s_name;
@@ -107,7 +96,7 @@ public:
         array = new int[iTableLen];
     }
 
-    CTable(const CTable &pcOther) {
+    CTable(const CTable &pcOther) { // kopiujacy
         s_name = pcOther.s_name + "_copy";
         tableSize = pcOther.tableSize;
         std::cout << "copy: " + s_name << std::endl;
@@ -115,6 +104,8 @@ public:
     }
 
     ~CTable() {
+        delete array;
+        tableSize = 0;
         std::cout << "usuwam: " + s_name << std::endl;
     }
 
@@ -133,7 +124,7 @@ public:
         }
         array = new int[iTableLen];
         tableSize = iTableLen;
-        std::cout << "we changed array size" << std::endl;
+        std::cout << "we changed array size bSetNewSize" << std::endl;
         return true;
     }
 
@@ -143,8 +134,8 @@ public:
         return clone;
     }
 
-    std::string print() {
-        std::cout << tableSize << std::endl;
+    std::string printTableSize() {
+        std::cout << "table size = "<< tableSize << std::endl;
     }
 };
 
@@ -154,30 +145,26 @@ std::string bool_to_string_converter(const bool b) {
 
 void v_mod_tab(CTable *pc_new_tab, int iNewSize) {
     pc_new_tab->bSetNewSize(iNewSize);
-    pc_new_tab->print();
 }
 
 void v_mod_tab(CTable c_tab, int iNewSize) {
     c_tab.bSetNewSize(iNewSize);
-    c_tab.print();
 }
-
 int main() {
     std::cout << "TASK 1" << std::endl;
-    int iSize = 2;
+    int iSize = 3;
     v_alloc_table_fill_34(iSize);
     std::cout << "\n" << std::endl;
 
     std::cout << "TASK 2" << std::endl;
-    int **pi_table; //    int **pi_table = new int*[iSizeX]; //    display_array(pi_table, I_SIZE_X, I_SIZE_Y);
-    bool alloc_result = b_alloc_table_2dim(pi_table, I_SIZE_X, I_SIZE_Y);
+    int ***pi_table; //allocate_array_data(pi_table, I_SIZE_Y, I_SIZE_X);
+    bool alloc_result = b_alloc_table_2dim(pi_table, I_SIZE_Y, I_SIZE_X);
     std::cout << "alloc result: " << std::endl;
     std::cout << bool_to_string_converter(alloc_result) << std::endl;
-    allocate_array_data(pi_table, I_SIZE_X, I_SIZE_Y);
     std::cout << "\n" << std::endl;
 
     std::cout << "TASK 3" << std::endl;
-    bool dealloc_result = b_dealloc_table_2_dim(pi_table, I_SIZE_X);
+    bool dealloc_result = b_dealloc_table_2_dim(pi_table, I_SIZE_Y, I_SIZE_X);
     std::cout << "dealloc result: " << std::endl;
     std::cout << bool_to_string_converter(dealloc_result) << std::endl;
     std::cout << "\n" << std::endl;
@@ -198,18 +185,30 @@ int main() {
     parameterConstr = new CTable("constr", 2);
     delete parameterConstr;
 
-
-    
     std::cout << "\n" << std::endl;
     int iNewSize = 4;
-    CTable c_tab; //static allo statyczna wywoluje konstruktor i dekonstruktor
-    CTable *pc_new_tab; // dynamic allo wywoluje konstruktor ale dekonstruktor wtedy gdy damy delete pc_new_tab;
-    pc_new_tab = c_tab.pcClone();
-    delete pc_new_tab;
-    v_mod_tab(c_tab, iNewSize); //zmodyfikuje
-//    v_mod_tab(*pc_new_tab, iNewSize); //nie zmodyfikuje
+    std::cout << "procedura 1:" << std::endl;
+    CTable *table1;
+    table1 = new CTable();
+    v_mod_tab(*table1, iNewSize); //nie zmodyfikuje, utworzy kopie obiektu
+    table1->printTableSize();
+
+    std::cout << "procedura 2:" << std::endl;
+    CTable *table2;
+    table2 = new CTable();
+    v_mod_tab(table2, iNewSize); //zmodyfikuje nie utowrzy kopii obiektu
+    table2->printTableSize();
 
     return 0;
 }
 
 
+
+
+
+//    CTable c_tab; //static allo statyczna wywoluje konstruktor i dekonstruktor
+//    CTable *pc_new_tab; // dynamic allo wywoluje konstruktor ale dekonstruktor wtedy gdy damy delete pc_new_tab;
+//    pc_new_tab = c_tab.pcClone();
+//    delete pc_new_tab;
+//    std::cout << "\n" << std::endl;
+//    delete pc_new_tab;
